a8bd5e667b050f7588f3ee845e99f145e3f51077
Nilecram <marcelin.ray@protonmail.com>
2025-02-11 13:28:04 +0100

    main test avec xor

---
+++ JavaNeuralNetwork/src/Main.java
@@ -0,0 +1,28 @@
+import Network.NeuralNetwork;
+import Network.ReLUActivation;
+import Network.SigmoidActivation;
+import Network.SoftmaxActivation;
+
+public class Main {
+    public static void main(String[] args) {
+        NeuralNetwork nn = new NeuralNetwork();
+
+        nn.addLayer(2, new SigmoidActivation());
+        //nn.addLayer(4, new SigmoidActivation());
+        nn.addLayer(1, new SigmoidActivation());
+
+        double[][] trainingInputs = {
+                {0, 0}, {0, 1}, {1, 0}, {1, 1}
+        };
+        double[][] trainingOutputs = {
+                {0}, {1}, {1}, {0}
+        };
+
+        nn.train(trainingInputs, trainingOutputs, 20000);
+
+        System.out.println("Prédictions :");
+        for (double[] input : trainingInputs) {
+            System.out.println(input[0] + " XOR " + input[1] + " = " + nn.forward(input)[0]);
+        }
+    }
+}
